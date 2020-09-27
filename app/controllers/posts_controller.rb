9d825d5127e3cbc30ef57c64dc8628647842f09d
Miguel Macias <miguelfernandomacias@gmail.com>
2020-09-27 18:09:43 -0400

    arreglo de create y rutas

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  http_basic_authenticate_with name: 'desafiovamoscontodo', password: ' XAHTJEAS23123%23', only: [:dashboard]
+
 
   def index
     @posts= Post.all
@@ -10,6 +10,9 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.create(title:params[:title],
+                        content:params[:content],
+                        image_url:params[:image_url])
   end
 
 end
